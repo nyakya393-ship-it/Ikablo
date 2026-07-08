cf3880a8a407e36632fbae9822c267637973c8bd
Sio <nyakya393@gmail.com>
2026-07-08 20:16:15 +0900

    Update ui.js

--- assets/js/ui.js
+++ assets/js/ui.js
@@ -1,1 +1,89 @@
+// =========================
+// UI Controller
+// =========================
 
+
+
+// アクティブタブ変更
+
+function setActiveTab(pageName){
+
+
+    const tabs =
+        document.querySelectorAll(
+            "[data-page]"
+        );
+
+
+
+    tabs.forEach(tab=>{
+
+
+        tab.classList.remove(
+            "active"
+        );
+
+
+
+        if(
+            tab.dataset.page === pageName
+        ){
+
+            tab.classList.add(
+                "active"
+            );
+
+        }
+
+
+    });
+
+
+}
+
+
+
+// タブ初期化
+
+function initTabs(){
+
+
+    const tabs =
+        document.querySelectorAll(
+            "[data-page]"
+        );
+
+
+
+    tabs.forEach(tab=>{
+
+
+        tab.addEventListener(
+            "click",
+            ()=>{
+
+
+                const page =
+                    tab.dataset.page;
+
+
+
+                loadPage(
+                    page
+                );
+
+
+
+                setActiveTab(
+                    page
+                );
+
+
+            }
+        );
+
+
+    });
+
+
+}
